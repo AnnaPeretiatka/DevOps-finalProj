7965a91172c3e0e906e3cf4ea118e97f7bda6060
AnnaPeretiatka <annaperetiatka@gmail.com>
2025-09-19 12:37:04 +0300

    more156

--- infra/terraform/app_release.tf
+++ infra/terraform/app_release.tf
@@ -119,6 +119,8 @@ resource "helm_release" "statuspage" {
   depends_on = [
     aws_eks_node_group.default,   
     helm_release.alb,
-    module.db               
+    module.db
+    null_resource.docker_build_push
+
   ]
 }
